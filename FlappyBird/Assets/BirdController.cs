91518bfcc326dba543855567866c47ee6986c7db
Tobiasz Duliniec <tduliniec@edu.cdv.pl>
2024-06-06 16:37:46 +0200

    More updates

--- FlappyBird/Assets/BirdController.cs
+++ FlappyBird/Assets/BirdController.cs
@@ -1,37 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
-
-public class Car
-{
-    public int Speed;
-    public string Model;
-
-    public string GetModel()
-    {
-        return Model;
-    }
-}
+using UnityEngine.SceneManagement;
 
 public class BirdController : MonoBehaviour
 {
     public Rigidbody2D rb2D;
     public Vector2 JumpForce;
 
+    public GameObject GameOverPanel;
+    public TextMeshProUGUI PointsTextureField;
+    public AudioSource audioSource;
+
+    public AudioClip hitSfx;
+    public AudioClip jumpSfx;
+    public AudioClip scoreSfx;
+
+    public static bool GameOver;
+    public static bool HasStarted;
+
+    public int Points;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!");
+        GameOver = false;
+        HasStarted = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        PointsTextureField.text = Points.ToString();
+        if (GameOver)
+            return;
         // Debug.Log("Hello World per frame");
         if (Input.GetButtonDown("Jump"))
         {
+            audioSource.clip = jumpSfx;
+            audioSource.Play();
+
+
+            if (HasStarted == false)
+            {
+                HasStarted = true;
+                rb2D.gravityScale = 1;
+
+            }
             rb2D.AddForce(JumpForce, ForceMode2D.Impulse);
         }
 
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameOver = true;
+        GameOverPanel.SetActive(true);
+
+        audioSource.clip = hitSfx;
+        audioSource.Play();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Points++;
+
+        audioSource.clip = scoreSfx;
+        audioSource.Play();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("FlappyBird"); // ³adowanie sceny pocz¹tkowej
+    }
+
 }
--- FlappyBird/Assets/MoveController.cs
+++ FlappyBird/Assets/MoveController.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 
 public class MoveController : MonoBehaviour
 {
     public float speed;
+
+    public float DespawnPointX;
+    public float SpawnPointX;
+
+    public float MinY;
+    public float MaxY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(transform.position.x, Random.Range(MinY, MaxY), transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (BirdController.GameOver || BirdController.HasStarted == false)
+            return;
         transform.position = new Vector3(transform.position.x + speed * Time.deltaTime,
             transform.position.y, transform.position.z); //pozycja to wektor3?
         //Time.deltaTime - czas pomiêdzy ostatni¹ a obecn¹ klatk¹
+
+        if (transform.position.x < DespawnPointX)
+            transform.position = new Vector3(SpawnPointX, Random.Range(MinY, MaxY), transform.position.z);
     }
 }
